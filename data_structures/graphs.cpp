23f03f3650048057a2b60cd59cf87461e9bdab1a
jgagnaire <jean.gagnaire@gmail.com>
2017-02-10 12:03:37 +0000

    corrected graph functions

--- data_structures/graphs.cpp
+++ data_structures/graphs.cpp
@@ -1,110 +1,147 @@
+
+#include <cstring>
 #include <list>
-#include <array>
-#include <stack>
 #include <queue>
+#include <stack>
+#include <vector>
+#include <array>
 #include <iostream>
+#include <bitset>
+#include <utility>
 
-#define GRAPH_SIZE 4
-
-struct graph
+class Graph
 {
-  std::list<int> *adjacency_list;
+public:
+  Graph(int v)
+    : vertices_nb(v), adj(new std::list<std::pair<int, int> >[v])
+  {
+  }
+
+  ~Graph()
+  {
+    delete[] adj;
+  }
+
+  void buildEdge(int vertex1, int vertex2, int weight)
+  {
+    adj[vertex1].push_back(std::make_pair(vertex2, weight));
+    adj[vertex2].push_back(std::make_pair(vertex1, weight));
+  }
+
+  void bfs()
+  {
+    bool visited[vertices_nb];
+    ::memset(&visited[0], 0, vertices_nb);
+
+    for (int a = 0; a < vertices_nb; ++a)
+      if (!visited[a])
+	sub_bfs(a, visited);
+    std::cout << std::endl;
+  }
+
+  void dfs_it()
+  {
+    bool visited[vertices_nb];
+    ::memset(&visited[0], 0, vertices_nb);
+
+    for (int a = 0; a < vertices_nb; ++a)
+      if (!visited[a])
+	sub_dfs_it(a, visited);
+    std::cout << std::endl;
+  }
+
+  void dfs_rec()
+  {
+    bool visited[vertices_nb];
+    ::memset(&visited[0], 0, vertices_nb);
+
+    for (int a = 0; a < vertices_nb; ++a)
+      if (!visited[a])
+	sub_dfs_rec(a, visited);
+    std::cout << std::endl;
+  }
+
+private:
+  void sub_dfs_rec(int elem, bool *visited)
+  {
+    std::cout << elem << " ";
+    visited[elem] = true;
+    for (auto i : adj[elem])
+      if (!visited[i.first])
+	sub_dfs_rec(i.first, visited);
+  }
+
+  void sub_bfs(int start, bool *visited)
+  {
+    std::queue<int> q;
+
+    q.push(start);
+    while (!q.empty())
+      {
+	int current = q.front();
+	q.pop();
+
+	if (!visited[current])
+	  {
+	    visited[current] = true;
+	    std::cout << current << " ";
+
+	    for (auto e : adj[current])
+	      if (!visited[e.first])
+		q.push(e.first);
+	  }
+      }
+  }
+
+  void sub_dfs_it(int start, bool *visited)
+  {
+    std::stack<int> s;
+
+    s.push(start);
+    while (!s.empty())
+      {
+	int current = s.top();
+	s.pop();
+
+	if (!visited[current])
+	  {
+	    visited[current] = true;
+	    std::cout << current << " ";
+
+	    for (auto e : adj[current])
+	      s.push(e.first);
+	  }
+      }
+  }
+
+  int vertices_nb;
+  std::list<std::pair<int, int> > *adj;
 };
 
-void bfs(graph *g, int elem)
+int main(void)
 {
-  std::queue<int> q;
-  std::array<bool, GRAPH_SIZE> visited;
+  Graph g(9);
 
-  visited.fill(false);
+  g.buildEdge(0, 1, 4);
+  g.buildEdge(0, 7, 8);
+  g.buildEdge(1, 7, 11);
+  g.buildEdge(1, 2, 8);
+  g.buildEdge(2, 3, 7);
+  g.buildEdge(2, 8, 2);
+  g.buildEdge(2, 5, 4);
+  g.buildEdge(3, 4, 9);
+  g.buildEdge(3, 5, 14);
+  g.buildEdge(4, 5, 10);
+  g.buildEdge(5, 6, 2);
+  g.buildEdge(6, 8, 6);
+  g.buildEdge(6, 7, 1);
+  g.buildEdge(7, 8, 7);
 
-  q.push(elem);
-  visited[elem] = true;
+  g.dfs_rec();
 
-  while (!q.empty())
-    {
-      elem = q.front();
-      std::cout << elem << " ";
-      q.pop();
+  g.dfs_it();
 
-      for (auto i : g->adjacency_list[elem])
-	if (!visited[i])
-	  {
-	    visited[i] = true;
-	    q.push(i);
-	  }
-    }
-}
+  g.bfs();
 
-void add_edge(graph *g, int vertex1, int vertex2)
-{
-  g->adjacency_list[vertex1].push_back(vertex2);
-}
-
-void sub_dfs_rec(graph *g, int elem, std::array<bool, GRAPH_SIZE> &visited)
-{
-  visited[elem] = true;
-  std::cout << elem << " ";
-
-  for (auto i : g->adjacency_list[elem])
-    if (!visited[i])
-      sub_dfs_rec(g, i, visited);
-}
-
-void dfs_rec(graph *g, int elem)
-{
-  std::array<bool, GRAPH_SIZE> visited; 
-
-  visited.fill(false);
-
-  sub_dfs_rec(g, elem, visited);
-}
-
-void dfs_it(graph *g, int elem)
-{
-  std::array<bool, GRAPH_SIZE> visited;
-  std::stack<int> s;
-
-  visited.fill(false);
-
-  s.push(elem);
-  while (!s.empty())
-    {
-      elem = s.top();
-      s.pop();
-
-      if (!visited[elem])
-	{
-	  std::cout << elem << " ";
-	  visited[elem] = true;
-
-	  for (auto j : g->adjacency_list[elem])
-	      s.push(j);
-	}
-    }
-}
-
-int  main()
-{
-  graph g;
-
-  g.adjacency_list = new std::list<int> [GRAPH_SIZE];
-
-  add_edge(&g, 0, 1);
-  add_edge(&g, 0, 2);
-  add_edge(&g, 1, 2);
-  add_edge(&g, 2, 0);
-  add_edge(&g, 2, 3);
-  add_edge(&g, 3, 3);
-
-  dfs_rec(&g, 2);
-  std::cout << std::endl;
-
-  dfs_it(&g, 2);
-  std::cout << std::endl;
-
-  bfs(&g, 2);
-  std::cout << std::endl;
-
-  delete[] g.adjacency_list;
+  return 0;
 }
