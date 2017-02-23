3323cb93e0130ac8a1f491cce7601b79c1f290c0
jgagnaire <jean.gagnaire@gmail.com>
2017-02-23 19:10:22 +0000

    modified 3sum

---
+++ algos/3sum.cpp
@@ -0,0 +1,49 @@
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+#define LEN 10
+
+void func(std::vector<int> const &v)
+{
+  for (unsigned long i = 0; i < v.size() - 2; ++i)
+    {
+      int a = v[i];
+      int left = i + 1;
+      int right = v.size() - 1;
+
+      while (left < right)
+	{
+	  int b = v[left];
+	  int c = v[right];
+
+	  if (c + b == -a)
+	    {
+	      std::cout << a << " + " << b << " + " << c << " = 0" << std::endl;
+	      ++left;
+	      --right;
+	    }
+	  else if (c + b > -a)
+	    --right;
+	  else
+	    ++left;
+	}
+    }
+}
+
+int	main()
+{
+  std::vector<int> tab;
+  int	a;
+
+  srand(time(0));
+  for (a = 0;a < LEN;++a)
+    {
+      tab.push_back(!(a % 2) ? (rand() % LEN) : -1 * (rand() % LEN));
+      std::cout << tab[a] << " ";
+    }
+  std::cout << std::endl;
+
+  func(tab);
+}
